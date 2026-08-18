c6d32a9faf06f35381ec0e03c0eff3f90bfc6741
306bobby-android <306bobby-android@users.noreply.github.com>
2026-08-18 15:32:22 -0400

    apparmor: add af_unix mediation
    Problem: Missing AF_UNIX mediation prevented D-Bus from obtaining peer security labels via SO_PEERSEC.
    Fix: Backport af_unix mediation and peer labeling from 4.19 reference kernel.

--- security/apparmor/include/path.h
+++ security/apparmor/include/path.h
@@ -18,6 +18,8 @@
 
 enum path_flags {
 	PATH_IS_DIR = 0x1,		/* path is a directory */
+	PATH_SOCK_COND = 0x2,		/* path is a unix socket, mediate
+					 * it conditionally (af_unix) */
 	PATH_CONNECT_PATH = 0x4,	/* connect disconnected paths to / */
 	PATH_CHROOT_REL = 0x8,		/* do path lookup relative to chroot */
 	PATH_CHROOT_NSCONNECT = 0x10,	/* connect paths that are at ns root */
