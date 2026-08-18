c664ff4dbfd20d16edefcef646ab1fd17ea44e1b
306bobby-android <306bobby-android@users.noreply.github.com>
2026-08-18 16:03:56 -0400

    apparmor: advertise af_unix feature
    Problem: apparmor_parser omitted unix rules because securityfs did not advertise features/network/af_unix.
    Fix: Expose features/network/af_unix in securityfs network features.

--- security/apparmor/include/net.h
+++ security/apparmor/include/net.h
@@ -97,6 +97,7 @@ struct aa_secmark {
 };
 
 extern struct aa_sfs_entry aa_sfs_entry_network[];
+extern struct aa_sfs_entry aa_sfs_entry_network_compat[];
 
 void audit_net_cb(struct audit_buffer *ab, void *va);
 int aa_profile_af_perm(struct aa_profile *profile, struct common_audit_data *sa,
