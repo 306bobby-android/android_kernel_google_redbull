ba88ad971aacc3b42a7f5df8de029560768fb188
306bobby-android <306bobby-android@users.noreply.github.com>
2026-08-19 15:51:01 -0400

    apparmor: keep af_unix peer labeling, drop fine grained unix mediation
    Problem: Fine-grained unix rules caused profile unpack errors in 4.19 policy parser.
    Fix: Retain peer labeling for SO_PEERSEC while bypassing fine-grained unix rule checks.

--- security/apparmor/include/net.h
+++ security/apparmor/include/net.h
@@ -80,9 +80,6 @@ struct aa_sk_ctx {
 ({						\
 	int __e;				\
 	switch ((FAMILY)) {			\
-	case AF_UNIX:				\
-		__e = aa_unix_ ## FN;		\
-		break;				\
 	default:				\
 		__e = DEF_FN;			\
 	}					\
@@ -97,7 +94,6 @@ struct aa_secmark {
 };
 
 extern struct aa_sfs_entry aa_sfs_entry_network[];
-extern struct aa_sfs_entry aa_sfs_entry_network_compat[];
 
 void audit_net_cb(struct audit_buffer *ab, void *va);
 int aa_profile_af_perm(struct aa_profile *profile, struct common_audit_data *sa,
